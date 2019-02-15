2183d532cd53cf53486e550d4e16c3c374d332df
Andrei <andrei-kom@mail.ru>
2019-02-15 17:55:34 +0200

    Session refresh using setInterval

--- app/jobs.js
+++ app/jobs.js
@@ -4,7 +4,7 @@ const zed = require('./main');
 var cron = require('node-cron');
 
 //Session refresh every 30 minutes
-cron.schedule('*/30 * * * *', () => {
+setInterval(function() {
     if (zed.manager._steam.steamID) {
         //console.log('Already logged in: ' + zed.manager.steam.steamID);
         zed.manager._steam.webLogOn();
@@ -13,7 +13,7 @@ cron.schedule('*/30 * * * *', () => {
         zed.manager._steam.logOn(logOnOptions);
         console.log('Logged in again using cron');
     }
-});
+}, 30 * 60 * 1000);
 
 //Update Prices every day at 1:30pm
 cron.schedule('30 13 * * 1,3,5,7', async () => {
