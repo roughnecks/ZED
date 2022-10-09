15fb73acd9a06a39af04d7839be036f8d7bcc79f
Simone <roughnecks@gmx.com>
2022-10-09 14:58:29 +0200

    Update manager.js

--- app/manager.js
+++ app/manager.js
@@ -47,7 +47,7 @@ async function processOffer(offer, them) {
             if (err) {
                 console.log(err);
             } else {
-                console.log(chalk.green('Donation accepted from' + offer.partner.getSteam3RenderedID() ));
+                console.log(chalk.green('Donation accepted from ' + them.personaName ));
                 manager._steam.chatMessage(offer.partner.getSteam3RenderedID(), 'Thanks for your generous donation!');
 
                 if (offer.itemsToReceive.length > 4) {
