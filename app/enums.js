660cdd02031174f20eb09adf927db2a83b85f35e
Simone <roughnecks@gmx.com>
2022-10-10 16:35:28 +0200

    removed booster item type

--- app/enums.js
+++ app/enums.js
@@ -6,14 +6,12 @@ const enums = {
         Card: 2,
         Background: 3,
         Emote: 4,
-        Booster: 5,
         Unknown: 99999999,
         properties:
         {
             2: { name: 'Card', value: 2 },
             3: { name: 'Background', value: 3 },
             4: { name: 'Emote', value: 4 },
-            5: { name: 'Booster', value: 5 },
             99999999: { name: 'Unknown', value: 99999999 }
         }
     }
