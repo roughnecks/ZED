ca58b5f04c41c652955d6d4937f68e873efe1e44
Simone <roughnecks@gmx.com>
2022-10-14 17:07:44 +0200

    added emote to bot comment for more than 50 items donated

--- app/manager.js
+++ app/manager.js
@@ -40,7 +40,7 @@ async function postComment(donator, donationnum) {
         manager._community.postUserComment(config.botSteamID3, 'Thanks ' + donator + ' for your kind contribution of ' + donationnum + ' Items! :steamhappy:');
         console.log(chalk.green('Comment Posted on Bot\'s Profile'));
     } else {
-        manager._community.postUserComment(config.botSteamID3, 'Thanks ' + donator + ' for your massive contribution of ' + donationnum + ' Items! :steamthumbsup:');
+        manager._community.postUserComment(config.botSteamID3, 'Thanks ' + donator + ' for your massive contribution of ' + donationnum + ' Items! :steamthumbsup: :csgoa:');
         console.log(chalk.green('Comment Posted on Bot\'s Profile'));
     }
 }
