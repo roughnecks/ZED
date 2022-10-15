ddb5b397dbb223e5ebfeaad40a46d106055abc7d
Simone <roughnecks@gmx.com>
2022-10-15 17:56:08 +0200

    added test job

--- app/jobs.js
+++ app/jobs.js
@@ -4,6 +4,11 @@ const zed = require('./main');
 
 const chalk = require('chalk');
 
+const schedule = require('node-schedule');
+
+const fs = require("fs");
+const path = require("path");
+
 //Session refresh every 30 minutes
 setInterval(function() {
     if (zed.manager._steam.steamID) {
@@ -14,3 +19,26 @@ setInterval(function() {
         console.log(chalk.yellow('Called Logon (setInterval)'));
     }
 }, 30 * 60 * 1000);
+
+
+
+//Delete all cooldowns at Midnight
+
+schedule.scheduleJob('58 17 * * *', () => {
+
+    const directory = "cooldown";
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        /*    
+            for (const file of files) {
+              fs.unlink(path.join(directory, file), (err) => {
+                console.log(file + " deleted");
+                if (err) throw err;
+              });
+            }
+        */
+        console.log(path);
+    });
+
+});
