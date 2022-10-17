270a5364fdc2244f4c4aebb03c849369a0c7e504
Simone <roughnecks@gmx.com>
2022-10-18 00:01:55 +0200

    added group functionality from main bot

--- app/client.js
+++ app/client.js
@@ -133,6 +133,27 @@ zed.manager._steam.on('friendRelationship', (steamID, relationship) => {
     }
 });
 
+//New Group Chat Message
+//This will fire when a new chat message gets posted in Group Chat we're in
+zed.manager._steam.chat.on('chatMessage', function (message) {
+    var senderID = message.steamid_sender;
+    var senderAccountID = senderID.accountid;
+    
+    //console.log(message);
+
+    zed.manager._steam.getPersonas([senderID], function (err, personas) {
+        if (!err) {
+            var sender = personas[senderID]["player_name"];
+            parseMessage(message.chat_group_id, message.chat_id, message.message_no_bbcode, senderID, senderAccountID, sender);
+        } else {
+            console.log(err);
+        }
+    });
+
+});
+
+
+
 //Commands
 zed.manager._steam.on('friendMessage', function (steamID, message) {
     if (message.startsWith('[tradeoffer sender=')) {
@@ -140,7 +161,7 @@ zed.manager._steam.on('friendMessage', function (steamID, message) {
     }
 
     if (message === "!help") {
-        zed.manager._steam.chatMessage(steamID, 'Hello, if you want me to sign your profile, type "!sign" without the quotes.');
+        zed.manager._steam.chatMessage(steamID, 'Hello, if you want me to sign your profile, type "!sign" without the quotes. Have a nice day!');
     }
     else if (message === "!sign") {
         zed.manager._community.postUserComment(steamID, 'ZED was here. :meltdownzed:');
@@ -158,3 +179,299 @@ zed.manager._steam.on('friendMessage', function (steamID, message) {
         zed.manager._steam.chatMessage(steamID, 'I don\'t understand any other command but "!help" and "!sign" (so far).');
     }
 });
+
+
+async function parseMessage(groupID, chatID, message, senderID, senderAccountID, sender) {
+    if (!message || !message.startsWith('!')) {
+        return;
+    }
+
+    if (message === "!hello") {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Hi there " + "[mention=" + senderAccountID + "]@" + sender + "[/mention]" + "!" + " :steamhappy:");  // [mention=accountid]@name[/mention]
+    } else if (message === "!next") {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Your satisfaction is our best prize. Next!");
+    } else if (message === "!help") {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I'm a Steam CHAT and Trading BoT; if you want to trade with me, first read the info showcase on my profile. For a list of available commands, type '!commands' without the quotes. More at: https://github.com/roughnecks/ZED" );
+    } else if (message === "!commands") {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "!csgo - Retrieve CS:GO User Stats" + "\n" + "!hello" + "\n" + "!help" + "\n" + "!next" + "\n" 
+        + "!quote add <text> | del <number> | <number> | info <number> | rand [nickname] | match <string> | last [nickname] - Group Quotes Management" 
+        + "\n" + "!tf2 <class> - Retrieve TF2 User Stats for selected Class" + "\n" + "!weather <city> <metric || imperial> - Ask the weatherman for location");
+    } else if (message.startsWith('!weather')) {
+        var str = message.substr(9);
+        var res = str.split(" ");
+        if (res.length > 1) {
+            var units = res[res.length - 1];
+            units = units.toUpperCase();
+            res.length = res.length - 1;
+            var city = res.join(' ');
+            checkWeather(city, units, groupID, chatID);
+        } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "You must specify a city and a unit of measure, either 'metric' or 'imperial'."); }
+    
+    
+    } else if (message.startsWith('!tf2')) {
+        var tf2class = message.substr(5);
+        if (tf2class) {
+            tf2Stats(tf2class, groupID, chatID, sender, senderID);
+        } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "You must specify a class."); }
+    
+    
+    } else if (message === "!csgo") {
+        csgoStats(groupID, chatID, sender, senderID);
+
+
+    } else if (message.startsWith('!quote')) {
+        var str = message.substr(7);
+        var res = str.split(" ");
+        if (res[0] === 'add') {
+            res.shift();
+            var quote = res.join(' ');
+            if (quote === "") {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Where's my quote!?");
+            }
+            else {
+                var date = new Date();
+                var seconds = Math.round(date.getTime() / 1000);
+                let senderID64 = senderID.getSteamID64();
+                var sequenceID = await db.getNextSequenceValue("quoteID");
+                var insertion = await db.insertQuote(sequenceID, sender, senderID64, quote, seconds, groupID, chatID);
+                if (insertion) {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Quote #" + sequenceID + " added.");
+                } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Some kind of error occurred. Quote wasn't added :("); }
+            }
+
+        } else if (res[0] === 'del') {
+            res.shift();
+            var quoteNum = res.join(' ');
+            quoteNum = Number(quoteNum);
+            if (isNaN(quoteNum) || (quoteNum === 0)) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I need a quote's number, starting from '1'.");
+                return;
+            }
+            let author = await db.quoteInfo(quoteNum);
+            let senderID64 = senderID.getSteamID64();
+            let ismod = await isMod(senderID, groupID);
+            if ((ismod === 30) || (ismod === 40) || (ismod === 50) || (senderID64 === author) || (senderID64 === zed.config.ownerSteamID64)) {
+                var deletion = await db.deleteQuote(quoteNum);
+                if (typeof deletion !== 'undefined') {
+                    if (deletion === 1) {
+                        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Quote #" + quoteNum + " deleted.");
+                    } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Quote not found"); }
+                } else {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Some kind of error occurred. Quote wasn't deleted :(");
+                }
+            } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Sorry, you're not a Group Mod nor the quote's Author."); }
+
+
+        } else if (str == "") {
+            zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I need a quote number, starting from '1' or any sub-command. See '!commands'.");
+        
+
+        } else if (!isNaN(Number(str))) {
+            var quoteNum = Number(str);
+            var quote = await db.findQuote(quoteNum);
+            if (quote) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, quote);
+            } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Quote not found."); }
+
+
+        } else if(res[0] === 'info') {
+            res.shift();
+            var quoteNum = res.join(' ');
+            quoteNum = Number(quoteNum);
+            if ( isNaN(quoteNum) || (quoteNum === 0) ) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I need a quote's number, starting from '1'.");
+                return;
+            }
+            let author = await db.quoteInfo(quoteNum);
+            let ismod = await isMod(senderID, groupID);
+            if ((ismod === 30) || (ismod === 40) || (ismod === 50)) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Author's SteamID64 is: " + author);
+            } else { 
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "You need to be a mod for such request.") 
+            }
+        
+
+        } else if (res[0] === 'rand') {
+            res.shift();
+            let match = res.join(' ');
+            if (match) {
+                let rand = await db.randQuote(match);
+                if (rand) {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, rand);
+                } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No Match."); }
+            } else {
+                let rand = await db.randQuote();
+                if (rand) {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, rand);
+                } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No Match."); }
+            }
+        
+
+        } else if (res[0] === 'match') {
+            res.shift();
+            let query = res.join(' ');
+            if (query) {
+                let output = await db.matchQuote(query);
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, output);
+            } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I need a string to search for."); }
+         
+         
+        } else if ( res[0] === 'last') {
+            res.shift();
+            let match = res.join(' ');
+            if (match) {
+                let last = await db.lastQuote(match);
+                if (last) {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, last);
+                } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No Match."); }
+            } else {
+                let last = await db.lastQuote();
+                if (last) {
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, last);
+                } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No Match."); }
+            }
+
+
+        } else { zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Not a valid sub-command or quote number."); }
+    }
+}
+
+
+async function checkWeather(city, units, groupID, chatID) {
+
+    var url;
+    var apiKey = zed.config.weatherAPI;
+
+    if (apiKey) {
+        if (units === 'METRIC') {
+            url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+        } else if (units === 'IMPERIAL') {
+            url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+        }
+        else {
+            zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Wrong or missing unit of measure.");
+            return;
+        }
+
+
+        try {
+            var response = await axios.get(url);
+            var weather = response.data;
+            //console.log(weather);
+            if (weather.cod === 200) {
+                if (units === 'METRIC') {
+                    let result = `It's ${weather.weather[0].description} and ${weather.main.temp} °C in ${weather.name}, ${weather.sys.country}! Pressure is ${weather.main.pressure} hPa, humidity is ${weather.main.humidity}% and wind speed is ${weather.wind.speed} meter/sec.`;
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, result);
+                } else if (units == 'IMPERIAL') {
+                    let result = `It's ${weather.weather[0].description} and ${weather.main.temp} °F in ${weather.name}, ${weather.sys.country}! Pressure is ${weather.main.pressure} hPa, humidity is ${weather.main.humidity}% and wind speed is ${weather.wind.speed} miles/hour.`;
+                    zed.manager._steam.chat.sendChatMessage(groupID, chatID, result);
+                }
+            }
+        } catch (e) {
+            //console.error(e);
+            if (e.response.status === 404) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "City not found.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            } else {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Houston, we have a problem! Check console.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            }
+        }
+    } else {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No API Key defined in config file, aborting.");
+        return;
+    }
+}
+
+
+async function tf2Stats(tf2class, groupID, chatID, sender, senderID) {
+    var apikey = zed.config.steamAPI;
+
+    if (apikey) {
+        var playerID64 = senderID.getSteamID64();
+        var player = sender;
+        var tf2classLower = tf2class.toLowerCase();
+        var tf2classCapitalized = tf2classLower.charAt(0).toUpperCase() + tf2classLower.slice(1);
+
+        var url = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key=${apikey}&steamid=${playerID64}`;
+
+
+        try {
+            var response = await axios.get(url);
+            var output = response.data;
+            var tf2Stats = new Tf2Stats();
+            //console.log(JSON.stringify(output))
+
+            tf2Stats.setStatsValues(tf2classCapitalized, output.playerstats.stats);
+            zed.manager._steam.chat.sendChatMessage(groupID, chatID, tf2Stats.getStatSummary(tf2classCapitalized, player));
+
+        } catch (e) {
+            //console.error(e);
+            if (e.response.status === 500) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Your Game Details are not Public.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            } else {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Houston, we have a problem! Check console.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            }
+        }
+
+    } else {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No API Key defined in config file, aborting.");
+        return;
+    }
+}
+
+async function csgoStats(groupID, chatID, sender, senderID) {
+
+    var apikey = zed.config.steamAPI;
+
+    if (apikey) {
+        var playerID64 = senderID.getSteamID64();
+        var player = sender;
+
+        var url = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=${apikey}&steamid=${playerID64}`;
+
+        try {
+            var response = await axios.get(url);
+            var output = response.data;
+            var csgoStats = new CSGOStats();
+            
+            csgoStats.setStatsValues(output.playerstats.stats);
+            zed.manager._steam.chat.sendChatMessage(groupID, chatID, csgoStats.getStatSummary(player));
+
+        } catch (e) {
+            //console.error(e);
+            if (e.response.status === 500) {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Your Game Details are not Public.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            } else {
+                zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Houston, we have a problem! Check console.");
+                console.log('Response Data = ' + (JSON.stringify(e.response.data)));
+                return;
+            }
+        }
+
+    } else {
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "No API Key defined in config file, aborting.");
+        return;
+    }
+}
+
+async function isMod(member, groupID) {
+    var response = await zed.manager._steam.chat.setSessionActiveGroups(groupID);
+    var memberID64 = member.getSteamID64();
+    var groupInfo = Object.values(response.chat_room_groups);
+    var members = groupInfo[0].members;
+    for(let element of members) {
+        var elementID64 = element.steamid.getSteamID64();
+        if (elementID64 === memberID64) {
+            return (element.rank);
+        }
+    }
+}
