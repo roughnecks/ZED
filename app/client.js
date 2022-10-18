a174a9811b08bfd9d550b4dadb4be1a97e4f8205
Simone <roughnecks@gmx.com>
2022-10-18 22:16:32 +0200

    try quote del again 5

--- app/client.js
+++ app/client.js
@@ -296,7 +296,7 @@ async function parseMessage(groupID, chatID, message, senderID, senderAccountID,
                 if (err) return console.log("No matching lines");
 
                 var replacement = `${quoteNum} + ": Quote deleted."`;
-                shell(`sed -i "s!${line}!${replacement}!" ${path}/quotes/quotedb`);
+                shell(`sed -i "s@${line}@${replacement}@" ${path}/quotes/quotedb`);
                 zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Quote 3 deleted.");
             });
         }
