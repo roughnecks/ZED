8906b7da5bca8def7553619e42fc34d82c134677
roughnecks <roughnecks@gmx.com>
2022-10-21 20:31:05 +0200

    changed radio command output

--- app/client.js
+++ app/client.js
@@ -199,7 +199,7 @@ async function parseMessage(groupID, chatID, message, senderID, senderAccountID,
     } else if (message === "!help") {
         zed.manager._steam.chat.sendChatMessage(groupID, chatID, "I'm a Steam CHAT and Trading BoT; if you want to trade with me, first read the info showcase on my profile. For a list of available commands, type '!commands' without the quotes. More at: https://github.com/roughnecks/ZED" );
     } else if (message === "!radio") {
-        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Listen to our StillStream Radio - Mixxxed Games Soundtracks: http://woodpeckersnest.space:8090/live");
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Listen to our StillStream Radio - Mixxxed Games Soundtracks. Use you favorite music player and connect to: http://woodpeckersnest.space:8090/live");
     } else if (message === "!commands") {
         zed.manager._steam.chat.sendChatMessage(groupID, chatID, "!hello" + "\n" + "!help" + "\n" + "!next" + "\n" + "!radio" + "\n" + "!csgo - Retrieve CS:GO User Stats" + "\n" 
         + "!tf2 <class> - Retrieve TF2 User Stats for selected Class" + "\n" + "!weather <city> <metric || imperial> - Ask the weatherman for location" + "\n" + 
