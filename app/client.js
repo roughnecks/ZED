d5b97bd3c35c53bc40e4bd074cac78cc8f5dc731
roughnecks <roughnecks@gmx.com>
2022-11-07 13:26:21 +0100

    fix to help file

--- app/client.js
+++ app/client.js
@@ -211,7 +211,7 @@ async function parseMessage(groupID, chatID, message, senderID, senderAccountID,
     } else if (message === "!np") {
         zed.manager._steam.chat.sendChatMessage(groupID, chatID, "Now Playing: :PlayMusic: " +  song);
     } else if (message === "!commands") {
-        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "!hello" + "\n" + "!help" + "\n" + "!next" + "\n" + "!radio" + "\n" + "!choose - Suggests you a game to play next" + "\n" + "!fortune [it | de | fr | es] - Fortunes in a few languages (EN without argument)" + "\n" + "!np - Now Playing on StillStream" + "\n" + 
+        zed.manager._steam.chat.sendChatMessage(groupID, chatID, "!hello" + "\n" + "!help" + "\n" + "!next" + "\n" + "!radio" + "\n" + "!choose - Suggests you a game to play next" + "\n" + "!fortune [it | de | fr | es] - Fortunes in a few languages (random without argument (en included))" + "\n" + "!np - Now Playing on StillStream" + "\n" + 
         "!csgo [SteamID64] - Retrieve CS:GO User Stats for yourself or optional given SteamID64" + "\n" 
         + "!tf2 <class> - Retrieve TF2 User Stats for selected Class" + "\n" + "!weather <city> <metric || imperial> - Ask the weatherman for location" + "\n" + 
         "!quote <add text> | <del number> | <info number> | <rand> - Quotes Management");    
