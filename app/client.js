f7b338e9321ed677e2e797eeb1638c0aa1010f41
roughnecks <roughnecks@gmx.com>
2023-05-30 22:21:45 +0200

    added Mumble info to convoy webhook

--- app/client.js
+++ app/client.js
@@ -839,7 +839,7 @@ setInterval(function () {
 
 setInterval(function () {
     var convoy = 'Join our ETS2 Dedicated Convoy Server: connect to the Convoy section in Euro Truck Simulator 2 and look for "[ITA][DEDICATO] Dumb & Dumber" in the list' + "\n" +
-    'Also visit our Trucksbook Company Profile over at https://trucksbook.eu/company/113842'
+    'Also visit our Trucksbook Company Profile over at https://trucksbook.eu/company/113842 and connect to "mumble.woodpeckersnest.space" for voice chat with Mumble while trucking around';
     webhook(convoy, "steam@chat.woodpeckersnest.space")
 }, 3500 * 60 * 1000);
 
